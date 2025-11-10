238c672a2615ac47d9a9f4ea970beed20260fb23
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-11-10 13:38:34 +0400

    Added baseic functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\SpecialtyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/doctors', [DoctorController::class, 'index'])->name('doctors.index');
+Route::get('/doctors/{doctor}', [DoctorController::class, 'show'])->name('doctors.show');
+
+Route::get('/organizations', [OrganizationController::class, 'index'])->name('organizations.index');
+Route::get('/organizations/{organization}', [OrganizationController::class, 'show'])->name('organizations.show');
+
+Route::get('/specialties', [SpecialtyController::class, 'index'])->name('specialties.index');
